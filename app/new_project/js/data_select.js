5d11304e1a3dc1d8c02b3248c04320b62470addc
Rohaanpk <rohaanpk03@gmail.com>
2021-10-26 22:42:40 +1300

    SearchBox Select Page

--- app/new_project/js/data_select.js
+++
@@ -1,6 +0,0 @@
-// const { ipcRenderer, webFrame, webContents } = require('electron')
-
-// ipcRenderer.on('load-url', (event, arg) => {
-//     document.getElementById('select_data').setAttribute('src', arg);
-//     console.log(arg);
-// })
