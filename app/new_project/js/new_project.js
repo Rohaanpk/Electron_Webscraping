b0b55479b6a2e704161a6b015c675959840e0d31
Rohaanpk <rohaanpk03@gmail.com>
2021-11-02 00:11:03 +1300

    SpreadSheet reader
    Added code to read data from spreadsheet

--- app/new_project/js/new_project.js
+++ app/new_project/js/new_project.js
@@ -1,12 +1,12 @@
 const { val } = require('cheerio/lib/api/attributes');
 const { text } = require('cheerio/lib/api/manipulation');
 const { ipcRenderer, webFrame, webContents, ipcMain } = require('electron')
+const fs = require("fs")
+const XLSX = require("xlsx")
 const text_array = []
 const img_array = []
 const webpreview = document.getElementById("web_preview");
-
-
-document.getElementById("file_input").addEventListener('input', load_new_page);
+const wbInput = document.getElementById('file_input');
 
 document.getElementById("preview").addEventListener("dom-ready", event => {
     var newProject = document.getElementById("new_project");
@@ -22,14 +22,13 @@ document.getElementById("preview").addEventListener("dom-ready", event => {
 //     ipcRenderer.send('scrapeurl', url);
 // })
 
-function check_url(str){
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(str);
+function check_url(str) {
+    try {
+        new URL(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 function previewsite(){
@@ -65,8 +64,8 @@ function selectsheet(){
 }
 
 function confirmsheet(){
-    document.getElementById('file_input').click();
-    // var selectedFile = document.getElementById('file_input').selectedFile;
+    wbInput.click();
+    // var workbook = XLSX.readFile(workbookpath)
     // print('test')
     // document.getElementById("select_sheet").style.display = "none";
     // document.getElementById("spreadsheet_preview").style.display = "inline";
@@ -74,6 +73,70 @@ function confirmsheet(){
     // var url = document.getElementById("input_url").value;
     // ipcRenderer.send('load-url', url);
 }
+
+// wbInput.addEventListener("change", async (evt) => {
+//     if (wbInput.files.length === 0)
+//       return;
+//     const columnA = []
+//     const file = wbInput.files[0];
+//     console.log(file)
+//     var data = new ArrayBuffer(file);
+//     const worksheet = XLSX.read(data);
+//     console.log(worksheet)
+//     load_new_page()
+
+//     for (let z in worksheet) {
+//         if(z.toString()[0] === 'A'){
+//           columnA.push(worksheet[z].v);
+//         }
+//       }
+      
+//     console.log(columnA);
+
+//     // do stuff with workbook here or pass it to another function 
+//   }, false);
+
+wbInput.addEventListener("change", (evt) => {
+    if (wbInput.files.length === 0)
+      return;
+  
+    actOnXLSX(wbInput.files[0]);
+  }, false);
+  
+  
+  async function actOnXLSX (file) {
+    const fileReader = new FileReader();
+  
+    const data = await new Promise((resolve, reject) => {
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+        fileReader.onerror = reject;
+  
+        fileReader.readAsArrayBuffer(file);
+      })
+      .finally(() => {
+        fileReader.onerror = fileReader.onload = null;
+      });
+  
+    const workbook = XLSX.read(data);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    console.log(worksheet)
+    
+    var columnA = []
+
+    for (let z in worksheet) {
+        if(z.toString()[0] === 'A'){
+          columnA.push(worksheet[z].v);
+        }
+      }
+      
+      console.log(columnA);
+  
+    // do stuff with workbook
+  }
+  
+
 
 function load_new_page(e) {
     var url = document.getElementById("input_url").value;
@@ -102,6 +165,25 @@ function new_img(){
     ipcRenderer.send('new_img_element', url);
 }
 
+function scrape_data() {
+
+    const columnA = [];
+
+    const columnA = Object.keys(worksheet).filter(x => /^A\d+/.test(x)).map(x => worksheet[x].v)
+    console.log(columnA)
+
+    console.log('scraping')
+
+    for (const element of txt_array) {
+        txt_download(element)
+    }
+}
+
+function txt_download(str) {
+    var scraped_text = document.document.getElementById(str).innerHTML;
+    // store scraped text in a external file based on location in arra
+  }
+
 ipcRenderer.on('load-url', (event, arg) =>{
     console.log(arg)
 })
@@ -127,7 +209,14 @@ ipcRenderer.on('img_xpath', (event, arg) => {
 
 ipcRenderer.on('text_xpath', (event, arg) => {
     text_array.push(arg);
-    console.log(text_array)
+    console.log(text_array);
+    const newDiv = document.createElement("p")
+    const newContent = document.createTextNode(arg)
+    newDiv.appendChild(newContent);
+    const currentDiv = document.getElementById("scraping_list");
+    newDiv.contentEditable = 'true'
+    currentDiv.insertBefore(newDiv, currentDiv.lastElementChild.nextSibling);
+
 })
 
 
@@ -141,3 +230,5 @@ ipcRenderer.on('text_xpath', (event, arg) => {
 //     var input = document.getElementById("file_input").value;
 //     ipcRenderer.send('scrapeurl', input)
 // })
+
+ 
